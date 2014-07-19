07f85682c4c51167a7cc739c0ec83e7d160175c5
laker <firmlyjin@gmail.com>
2014-07-19 14:01:48 +0800

    "修改正确的shell路径，使shell scipt能执行"

--- action/testssh.py
+++ action/testssh.py
@@ -7,44 +7,7 @@ from os.path import  join
 from os import getcwd
 from Subpros import subpros
 
-
-@url("/")
-def hello(environ, start_response):
-    s = "Hello, World!\n"
-
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(s)))
-    ])
-
-    return s
-
-
-@url("/async")
-def async(environ, start_response):
-    sleep(0.01)
-    s = "Hello, World!\n"
-
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(s)))
-    ])
-
-    return s
-
-
-@url("/remote")
-def remote(environ, start_response):
-    s = urlopen("http://220.181.40.167:8080").read()
-
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(s)))
-    ])
-
-    return s
-
-@url("/testssh")
+@url("/testssh2")
 def testssh(environ, start_response):
     sleep(0.01)
     curDir = getcwd()        
